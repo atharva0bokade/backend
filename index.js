729a97b40fbd803a8233cc9656af2eae478b0691
Atharva Bokade <atharva1bokade@gmail.com>
2022-06-01 18:01:39 +0530

    new

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 const dataModel = require("./models/UserData");
-const adjustschema = require("./models/Adjust");
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 3000;
+
 // const infoModel = require("./models/info")
 
+console.log("It is Working");
 mongoose.connect("mongodb+srv://atharva1bokade:456abc123@information.hwmdy.mongodb.net/information?retryWrites=true&w=majority",
  {
     useNewUrlParser: true,
 });
+
 
 app.post("/add", async (req,res)=>{
     const userName = req.body.userName;
@@ -34,7 +36,6 @@ try{
 app.put("/approve", async (req,res)=>{
     const new_approve = req.body.approve;
     const id=req.body.id;
-    
     
     try{
         await dataModel.findById(id,(err,update_appr)=>{
@@ -77,7 +78,7 @@ app.get("/rejected",  (req,res)=>{
    });
    
    
-   
+   console.log("It is Working");
  app.get("/requests1",  (req,res)=>{
     dataModel.find({time:"9am to 10am",approve:"Not Approved"},(err,result)=>{
    if(err){
@@ -132,41 +133,6 @@ app.get("/requests2",  (req,res)=>{
     });
    
    });
-   app.put("/email_sending", async (req,res)=>{
-    const new_email_send = req.body.email_send;
-    const id=req.body.id;
-    
-    
-    try{
-        await dataModel.findById(id,(err,update_email_send)=>{
-                update_email_send.email_send = new_email_send;
-                update_email_send.save();
-                res.send("Updated");
-        });
-   
-} catch(err){
-    console.log(err);
-}
-});
-   app.get("/email_s",  (req,res)=>{
-    dataModel.find({email_send:"Elligible"},(err,result)=>{
-   if(err){
-       res.send(err);
-   }
-     res.send(result);
-    });
-   
-   });
-   app.get("/time_cancel",  (req,res)=>{
-    adjustschema.find({},(err,result)=>{
-   if(err){
-       res.send(err);
-   }
-     res.send(result);
-    });
-   
-   });
-   
 //    app.get("/status",  (req,res)=>{
 //     dataModel.find({userName:req.body.name},(err,result)=>{
 //    if(err){
@@ -176,6 +142,7 @@ app.get("/requests2",  (req,res)=>{
 //     });
    
 //    });
+console.log(port);
 app.listen(port, ()=>{
     console.log("Server running on port "+port);
 });
